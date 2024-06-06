19191d2a9807f98e83c2b64cb4ae2a8c7258bcb7
Tommy840602 <tommy840602@gmail.com>
2024-06-06 17:51:57 +0800

    Add files via upload

---
+++ my_addr_book.pb
@@ -0,0 +1,7 @@
+
+E
+
+G. T. Wang{guozhao.wang@gmail.com"
+0912-345678"
+
+06-1234567
---
+++ write.py
@@ -0,0 +1,30 @@
+import addressbook_pb2
+import sys
+
+# 指定 Protocol Buffer 資料檔
+my_pb_file = "my_addr_book.pb"
+
+# 建立 AddressBook
+address_book = addressbook_pb2.AddressBook()
+
+# 增加一筆 Person 資料
+person = address_book.people.add()
+
+# 設定 Person 基本資料
+person.id = 123
+person.name = "G. T. Wang"
+person.email = "guozhao.wang@gmail.com"
+
+# 新增第一筆電話
+phone_number = person.phones.add()
+phone_number.number = "0912-345678"
+phone_number.type = addressbook_pb2.Person.MOBILE
+
+# 新增第二筆電話
+phone_number = person.phones.add()
+phone_number.number = "06-1234567"
+phone_number.type = addressbook_pb2.Person.WORK
+
+# 寫入 AddressBook
+with open(my_pb_file, "wb") as f:
+  f.write(address_book.SerializeToString())
